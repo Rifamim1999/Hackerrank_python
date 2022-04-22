b6d8a4b4d46e66d0ead9852aa14912b21db84662
Rifa <68515313+Rifamim1999@users.noreply.github.com>
2022-04-23 02:41:08 +0600

    Create Designer_door.py
        Size: 7 x 21 
        ---------.|.---------
        ------.|..|..|.------
        ---.|..|..|..|..|.---
        -------WELCOME-------
        ---.|..|..|..|..|.---
        ------.|..|..|.------
        ---------.|.---------

---
+++ Designer_door.py
@@ -0,0 +1,4 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N, M = map(int, input().split())
+pattern = [('.|.'*(i*2+1)).center(M, '-') for i in range(N//2)]
+print('\n'.join(pattern+['WELCOME'.center(M,'-')]+(pattern[::-1])))
